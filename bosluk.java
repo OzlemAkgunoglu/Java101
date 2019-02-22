58bb676b437afce0f791817e01c02fd717d86bb9
Ozlem Tugrul <OzlemTugrul@users.noreply.github.com>
2019-02-22 14:27:16 +0200

    Add files via upload

---
+++ bosluk.java
@@ -0,0 +1,29 @@
+/*Girilen bir cümledeki boşluk sayısını bulan programın kodunu yazınız.
+Örnek çıktı  ----   Bosluk sayisini bulmak istediğiniz cumleyi giriniz
+					Merhaba Dunya Burasi Github
+					Bu cumledeki boşluk sayısı: 3 */
+import java.util.*;
+public class bosluk
+{
+	public static void main(String[] args) {
+		Scanner klavye=new Scanner(System.in);
+		int deger, uzunluk, i, sayac=0;
+		String cumle, harf;
+		System.out.println("Bosluk sayisini bulmak istediğiniz cumleyi giriniz");
+		cumle = klavye.nextLine();
+		uzunluk=cumle.length();
+		i=uzunluk;
+		while(i>0)
+			{
+				harf=cumle.substring(uzunluk-i,uzunluk-i+1);
+				deger=harf.indexOf(" ");
+				if (deger!=-1) 
+				{
+					sayac=sayac+1;
+				}
+				i--;
+			}
+		System.out.println("Bosluk sayisi="+ sayac);
+		
+	}
+}
